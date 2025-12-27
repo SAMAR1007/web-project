b27f0539352a90c53bef1f3b2d6bb3aaa2d49fa4
SAMAR1007 <bistsamar999@gmail.com>
2025-12-27 18:18:30 +0545

    app setup

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "@/components/layout/navbar"
+import { Hero } from "@/components/home/hero"
+import { FeaturedProducts } from "@/components/home/featured-products"
+import { Categories } from "@/components/home/categories"
+import { Stats } from "@/components/home/stats"
+import { Footer } from "@/components/layout/footer"
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <Stats />
+      <Categories />
+      <FeaturedProducts />
+      <Footer />
+    </div>
+  )
+}
