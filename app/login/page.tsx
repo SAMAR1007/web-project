4f6d8520b4c550963df75f6cb9ebceb555123dab
SAMAR1007 <bistsamar999@gmail.com>
2026-03-08 20:50:58 +0545

    add comment to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+// Login page with centered form layout
 import { LoginForm } from "@/components/auth/login-form"
 import { Navbar } from "@/components/layout/navbar"
 import { Footer } from "@/components/layout/footer"
