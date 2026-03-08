a62c9111b60d3df0b80445e7cb97184688eb835a
SAMAR1007 <bistsamar999@gmail.com>
2026-03-08 20:51:02 +0545

    add comment to navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+// Main navigation bar with responsive mobile menu
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Hexagon, ShoppingCart, Menu, LayoutDashboard } from "lucide-react"
